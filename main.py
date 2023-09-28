bff09b16b7ba5b81387bbcdda381e635e832ff03
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2023-09-28 18:03:50 +0900

    refactor: change the structure of the code without changing its behavior

--- main.py
+++ main.py
@@ -1,92 +1,88 @@
 import os
 import csv
 import datetime
-import requests
 from time import sleep
 from random import randint
-from googleapiclient.discovery import build
+from requests import get
 from bs4 import BeautifulSoup
-from langchain.llms import ChatOpenAI  # ChatOpenAIをインポート
+from langchain.chat_models import ChatOpenAI
 
-# API設定
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
-CUSTOM_SEARCH_ENGINE_ID = os.environ.get("CUSTOM_SEARCH_ENGINE_ID")
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+# 環境変数を取得する関数
+def get_env_var(var_name):
+    var_value = os.getenv(var_name)
+    if var_value is None:
+        raise ValueError(f"{var_name} is not set")
+    return var_value
 
-# データ保存用ディレクトリ
-DATA_DIR = 'data'
+# GPT-3のインスタンスを初期化する関数
+def initialize_gpt3(api_key, model_name):
+    return ChatOpenAI(api_key=api_key, model=model_name)
 
-# ディレクトリ作成関数
-def makeDir(path):
-    if not os.path.isdir(path):
-        os.mkdir(path)
-
-# Google検索関数
-def getSearchResponse(keyword):
+# GPT-3を使用して検索キーワードを生成する関数
+def generate_search_keywords(gpt3_instance, input_word):
     try:
-        service = build("customsearch", "v1", developerKey=GOOGLE_API_KEY)
-        response = service.cse().list(
-            q=keyword,
-            cx=CUSTOM_SEARCH_ENGINE_ID,
-            lr='lang_ja',
-            num=10
-        ).execute()
-        return response['items']
-    except Exception as e:
-        print(f"An error occurred while searching: {e}")
-        return []
-
-# 要約関数
-def getSummary(content):
-    try:
-        model = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo-16k")  # ChatOpenAIのインスタンスを作成
         messages = [
-            {"role": "system", "content": "あなたは優秀な要約アシスタントです。入力された文章を適切な形で要約してください。"},
-            {"role": "user", "content": content}
+            {"role": "system", "content": "あなたは優秀な検索アシスタントです。入力された単語に関連する検索キーワードをできるだけ多く生成し、リスト形式で返します。"},
+            {"role": "user", "content": input_word}
         ]
-        completion = model.chat(messages, temperature=0, max_tokens=200)
-        return completion['message']['content']
-    except Exception as e:
-        print(f"An error occurred while generating summary: {e}")
-        return ""
-
-# CSVファイルを開く
-with open('search_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["Keyword", "Title", "URL", "Summary"])
-
-    # 検索ワード生成
-    try:
-        model = ChatOpenAI(api_key=openai_api_key, model="gpt-3.5-turbo-16k")  # ChatOpenAIのインスタンスを作成
-        messages = [
-            {"role": "system", "content": "You are an excellent search assistant. Generate as many relevant search keywords as possible for the input word and return them in list format."},
-            {"role": "user", "content": "python"}
-        ]
-        completion = model.chat(messages, temperature=0, max_tokens=500)
-        search_keywords = completion['message']['content'].split(", ")
+        completion = gpt3_instance.chat(messages)
+        return completion['choices'][0]['message']['content'].split(", ")
     except Exception as e:
         print(f"An error occurred while generating keywords: {e}")
-        search_keywords = []
+        return []
 
-    # 検索と結果の保存
-    for keyword in search_keywords:
-        print(f"Searching for: {keyword}")
-        search_results = getSearchResponse(keyword)
-        for result in search_results:
-            title = result['title']
-            url = result['link']
-            try:
-                response = requests.get(url)
-                website_content = response.text
-            except Exception as e:
-                print(f"An error occurred while getting website content: {e}")
-                continue
-            soup = BeautifulSoup(website_content, 'html.parser')
-            website_content = soup.get_text().replace('\n', '').replace(' ', '').replace('\t', '').replace('\r', '')
-            summary = getSummary(website_content)
-            date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            csvwriter.writerow([keyword, title, url, summary, date])
-            print(f"Title: {title}")
-            print(f"URL: {url}")
-            print(f"Summary: {summary}")
-            sleep(randint(2, 5))
+# ウェブサイトをフェッチし、パースする関数
+def fetch_and_parse_website(url):
+    try:
+        response = get(url)
+        if 'cloudflare' in response.headers.get('Server', '').lower():
+            print(f"Skipping {url} as it is protected by Cloudflare")
+            return ""
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup.get_text().replace('\n', '').replace(' ', '').replace('\t', '').replace('\r', '')
+    except Exception as e:
+        print(f"An error occurred while getting website content: {e}")
+        return ""
+
+# CSVファイルに書き込む関数
+def write_to_csv(csv_writer, keyword, title, url, summary):
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    csv_writer.writerow([keyword, title, url, summary, date])
+
+# 要約関数（未実装）
+# def get_summary(content):
+#     pass
+
+# メインの関数
+def main():
+    GOOGLE_API_KEY = get_env_var("GOOGLE_API_KEY")
+    CUSTOM_SEARCH_ENGINE_ID = get_env_var("CUSTOM_SEARCH_ENGINE_ID")
+    OPENAI_API_KEY = get_env_var("OPENAI_API_KEY")
+
+    gpt3 = initialize_gpt3(OPENAI_API_KEY, "gpt-3.5-turbo-16k")
+
+    search_keywords = generate_search_keywords(gpt3, "python")
+
+    visited_urls = set()
+
+    with open('search_results.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["Keyword", "Title", "URL", "Summary"])
+
+        for keyword in search_keywords[:10]:
+            print(f"Searching for: {keyword}")
+            search_results = []  # Replace with actual search function
+            for result in search_results[:10]:
+                title = result['title']
+                url = result['link']
+                if url in visited_urls:
+                    print(f"Skipping {url} as it has already been visited")
+                    continue
+                visited_urls.add(url)
+                website_content = fetch_and_parse_website(url)
+                summary = ""  # Replace with actual summary function
+                write_to_csv(csvwriter, keyword, title, url, summary)
+                sleep(randint(2, 5))
+
+if __name__ == "__main__":
+    main()
